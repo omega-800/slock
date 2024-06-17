4267b2d175940500e288a52ec3eebe29075da7e5
gs2 <georgiy.shevoroshkin@inteco.ch>
2024-06-17 08:04:16 +0200

    alternating colors

--- slock.c
+++ slock.c
@@ -308,7 +308,7 @@ readpw(Display *dpy, struct xrandr *rr, struct lock **locks, int nscreens,
 				}
 				break;
 			}
-			color = len ? (caps ? CAPS : INPUT) : (failure || failonclear ? FAILED : INIT);
+			color = len ? (caps ? CAPS : (len%2 ? INPUT : INPUT_ALT)) : ((failure || failonclear) ? FAILED : INIT);
 			if (running && oldc != color) {
 				for (screen = 0; screen < nscreens; screen++) {
 					XSetWindowBackground(dpy,
