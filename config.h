56e5114cdd30136c2c93a768561c8fa5ea624e26
Georgiy Shevoroshkin <georgiy.shevoroshkin@inteco.ch>
2024-07-23 21:58:11 +0200

    something cool

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "omega";
+static const char *group = "omega";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
@@ -8,16 +8,6 @@ static const char *colorname[NUMCOLS] = {
 	[INPUT_ALT] = "#227799", /* during input, second color */
 	[FAILED] = "#CC3333",   /* wrong password */
 	[CAPS] = "red",         /* CapsLock on */
-};
-
-/*
- * Xresources preferences to load at startup
- */
-ResourcePref resources[] = {
-		{ "color0",       STRING,  &colorname[INIT] },
-		{ "color4",       STRING,  &colorname[INPUT] },
-		{ "color1",       STRING,  &colorname[FAILED] },
-		{ "color3",       STRING,  &colorname[CAPS] },
 };
 
 /* treat a cleared input like a wrong password (color) */
@@ -31,3 +21,15 @@ static const char * text_color = "#ffffff";
 
 /* text size (must be a valid size) */
 static const char * font_name = "6x10";
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "color0",       STRING,  &colorname[INIT] },
+		{ "color4",       STRING,  &colorname[INPUT] },
+		{ "color5",       STRING,  &colorname[INPUT_ALT] },
+		{ "color1",       STRING,  &colorname[FAILED] },
+		{ "color3",       STRING,  &colorname[CAPS] },
+		{ "color9",       STRING,  &text_color },
+};
